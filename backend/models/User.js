b6e87903a3dde74f69f170c07bcbae556ac01d48
lad-bhargav <ladshiv867@gmail.com>
2025-03-07 21:36:21 +0530

    improvement in code

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,8 +1,19 @@
-const mongoose=require('mongoose')
-const { type } = require('os')
 
-const UserSchema=mongoose.Schema({
-    username:{type:String,require:true},
-    email:{type:String,require:true},
-    password:{type:String,require:true}
-})
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
+});
+
+export const UserModel = mongoose.model("users", UserSchema);
+
+// const mongoose=require('mongoose')
+// const { type } = require('os')
+
+// const UserSchema=mongoose.Schema({
+//     username:{type:String,require:true},
+//     email:{type:String,require:true},
+//     password:{type:String,require:true}
+// })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,24 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./components/navbar";
+import { Auth } from "./pages/auth";
+import { CreateRecipe } from "./pages/create-recipe";
+import { Home } from "./pages/home";
+import { SavedRecipes } from "./pages/saved-recipes";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const user = localStorage.getItem("username");
-    setIsLoggedIn(!!user);
-  }, []);
-
   return (
-    <Router>
-      <Navbar isLoggedIn={isLoggedIn} />
-      <Routes>
-        <Route path="/" element={<Home /> } />
-        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/register" element={<Register setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/home" element={ <Home /> } />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/saved-recipes" element={<SavedRecipes />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
